bfbece7ecaaeae5d418eb8bec990f07f29a62b54
Loremazze <mazzeilorenzo35@virgilio.it>
2023-10-19 21:17:09 +0200

    File Reading Test

--- src/main/java/it/unipi/mircv/App.java
+++ src/main/java/it/unipi/mircv/App.java
@@ -12,8 +12,8 @@ import java.util.ArrayList;
 public class App
 {
     public static void main( String[] args ) throws IOException {
-        //testBlock(0);
-        Index index = new Index("test_collection.tsv");
+        testBlock(0);
+        //Index index = new Index("test_collection.tsv");
     }
 
     public static void testBlock(int numberOfBlocks) throws IOException {
@@ -25,12 +25,11 @@ public class App
 
 
         byte[] buffer = null;
-        int bytesRead = 4;
         int termByteLength = 64;
         int offsetByteLength = 4;
         int elementOfListByteLength = 4;
         int bytesToRead = 64; // Specify the number of bytes you want to read
-        int offsetIncrement = 0;
+        int offset = 0;
         int currentOffset = 0;
         int followingOffset = 0;
 
@@ -51,38 +50,40 @@ public class App
         int count = 0;
         while (true) {
 
-            String striga = "Hello World!";
-            ByteBuffer stringaBuffer = ByteBuffer.allocate(64);
-            stringaBuffer.put(striga.getBytes());
-            FileOutputStream fos = new FileOutputStream("test.dat");
-            fos.write(stringaBuffer.array());
-
-            FileInputStream test = new FileInputStream("test.dat");
-            BufferedInputStream testBuff = new BufferedInputStream(test);
-            bytesRead = testBuff.read(buffer, offsetIncrement, offsetIncrement + termByteLength); //leggo il primo int
-
-            String termTest = new String(buffer,StandardCharsets.UTF_8);
-            System.out.println(termTest.charAt(10));
-
-            if (count == 0) return;
-            bytesRead = lexiconBufferedInputStream.read(buffer, offsetIncrement, offsetIncrement + termByteLength); //leggo il primo int
+            /*bytesRead = lexiconBufferedInputStream.read(buffer, offsetIncrement, termByteLength); //leggo il primo int
 
             if (bytesRead == -1) {
                 System.out.println("File finito");
                 return;
-            }
+            }*/
 
-            System.out.println(bytesRead);
-            String term = new String(buffer,StandardCharsets.UTF_8);
-            terms.add(term);
-            //terms.add(count,String.valueOf(lexiconBufferedInputStream.read(buffer, offsetIncrement, bytesRead + offsetIncrement)));
-            offsetIncrement += termByteLength;
-            System.out.println(terms.get(0));
+            RandomAccessFile lexiconFile = new RandomAccessFile(directoryPath + lexiconPath + "0.dat", "r");
+            RandomAccessFile docIdFile = new RandomAccessFile(directoryPath + docIdPath + "0.dat", "r");
+            RandomAccessFile termFreqFile = new RandomAccessFile(directoryPath + termFreqPath + "0.dat", "r");
+
+            // Position (offset) we start reading from
+            offset = 164; // We start reading from position 0
+            // Seek to the desired position
+            docIdFile.seek(offset);
+            // Read data from that position
+            byte[] bufferTest = new byte[4]; // Define a buffer to hold the data
+            int numberOfBytesRead = docIdFile.read(bufferTest);
+
+            System.out.println("Numero di bytes letti: " + numberOfBytesRead);
+
+            offset += termByteLength;
+
+            int result = ByteBuffer.wrap(bufferTest).getInt();
+            System.out.println("docId: " + result);
+            docIdFile.close();
+            if (count == 0) break;
+
+            /*
             currentOffset = lexiconBufferedInputStream.read(buffer, offsetIncrement, offsetIncrement + offsetByteLength);
             followingOffset = lexiconBufferedInputStream.read(buffer, offsetIncrement + termByteLength, offsetIncrement + offsetByteLength);
 
             postingList.add(count,readPostingList(currentOffset,followingOffset,buffer,docIdBufferedInputStream,termFreqBufferedInputStream));
-        }
+        */}
 
     }
 
@@ -102,6 +103,26 @@ public class App
 
         return postingList;
     }
+
+    /*
+    public void testWord() throws FileNotFoundException {
+
+        int[] buffer = new int[64];
+
+        String striga = "Hello World!";
+        ByteBuffer stringaBuffer = ByteBuffer.allocate(64);
+        stringaBuffer.put(striga.getBytes());
+        FileOutputStream fos = new FileOutputStream("test.dat");
+        fos.write(stringaBuffer.array());
+
+        FileInputStream test = new FileInputStream("test.dat");
+        BufferedInputStream testBuff = new BufferedInputStream(test);
+        bytesRead = testBuff.read(buffer, offsetIncrement, offsetIncrement + termByteLength); //leggo il primo int
+
+        String termTest = new String(buffer,StandardCharsets.UTF_8);
+        System.out.println(termTest.charAt(10));
+
+    }*/
 }
 
 
