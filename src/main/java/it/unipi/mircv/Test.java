02f5688fb44aab8642ddc70c772a0701ad822cb4
Loremazze <mazzeilorenzo35@virgilio.it>
2023-10-19 16:35:40 +0200

    Test class merge writing

--- src/main/java/it/unipi/mircv/Test.java
+++ src/main/java/it/unipi/mircv/Test.java
@@ -11,6 +11,7 @@ public class Test{
     public static void main(String[] args){
         //Classe per testare funzioni
         //Test bytes
+        /*
         byte[] data = "à".getBytes();
         int dataLength = data.length;
         int a= 2;
@@ -51,8 +52,80 @@ public class Test{
         String s = new String(stringByte, StandardCharsets.UTF_8);
         System.out.println("Stringa "+i+": "+s);
 
+        }*/
+
+        merge();
+    }
+
+    public void merge() {
+            HashMap<String, PostingList> index = new HashMap<String, PostingList>();
+
+            //File file = new File("C:/temp/test.txt");
+            //byte[] bytes = new byte[(int) fiepath.length()];
+            //FileInputStream fis = new FileInputStream(file);
+
+            String filePath = "example.txt"; // Replace with your file path
+            byte[] buffer = null;
+            int bytesRead = 4;
+            int bytesToRead = 0; // Specify the number of bytes you want to read
+            int postingListLength = 0;
+            int offset = 0;
+            String term = null;
+            buffer = new byte[bytesToRead];
+
+            FileInputStream fileInputStream = new FileInputStream(filePath);
+            BufferedInputStream bufferedInputStream = new BufferedInputStream(fileInputStream);
+
+            //fai un array di bufferedInputStream
+            for (allfiles) {  //indice senza Lexicon
+                //alloca reader nell'array
+                //(meglio fare un block descriptor?)
+            }
+
+
+
+            while (true) {
+                String term = getMinTermFromBlockReading();
+
+                bytesRead = bufferedInputStream.read(buffer, offset, offset + 4); //leggo il primo int
+                offset++;
+                term = String.valueOf(bufferedInputStream.read(buffer, offset, bytesRead + offset));
+                offset += bytesRead;
+                postingListLength = bufferedInputStream.read(buffer, offset, offset + 4);
+            }
+
+            while (bytesRead != -1) { //controlla se ho raggiunto la fine del file
+                bytesRead = bufferedInputStream.read(buffer, offset, offset + 4); //leggo il primo int
+                offset++;
+                term = String.valueOf(bufferedInputStream.read(buffer, offset, bytesRead + offset));
+                offset += bytesRead;
+                postingListLength = bufferedInputStream.read(buffer, offset, offset + 4);
+            }
         }
 
+        public String getMinTermFromBlockReading() {
+            String minTerm = "";
+            int count = 0;
+            for (BlockDescriptor blockDescriptor: Blocks) {
+                if (blockdescriptor.term < minTerm) {
+                    minTerm = blockdescriptor.term;
+                    count = 0;
+                }
+                else if (blockDescriptor.term == minTerm)
+                    count++;
+            }
+
+            if (count == 0) computePostingList(blockReader);
+            else
+
+            return "prova";
+        }
+
+        public void computePostingList(blockReader) {
+            for (quanto) {
+            bytesRead = bufferedInputStream.read(buffer, offset, offset + 4); //leggo il primo int
+
+        }
 
     }
 }
