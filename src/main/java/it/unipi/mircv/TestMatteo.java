1327c218ab0ca180862c23a098a7a9195d8d0e03
matte <matteomanni1999@gmail.com>
2023-11-03 22:36:52 +0100

    implemented variableByte compression

--- src/main/java/it/unipi/mircv/TestMatteo.java
+++ src/main/java/it/unipi/mircv/TestMatteo.java
@@ -1,14 +1,15 @@
 package it.unipi.mircv;
 
-import it.unipi.mircv.File.DocumentIndexHandler;
-import it.unipi.mircv.Index.BlockMerger;
-import it.unipi.mircv.Index.Index;
+import it.unipi.mircv.Index.Compression;
 
 import java.io.IOException;
-import java.nio.channels.FileChannel;
+
 
 public class TestMatteo {
     public static void main(String[] args) throws IOException {
+
+        /*
+        //test DocumentIndexHandler
         Index index = new Index("test_collection.tsv");
         int numberOfBlocks = index.getNumberOfBlocks();
         BlockMerger blockMerger = new BlockMerger(numberOfBlocks);
@@ -17,5 +18,21 @@ public class TestMatteo {
         DocumentIndexHandler documentIndexHandler = new DocumentIndexHandler();
         System.out.println("document length: " + documentIndexHandler.readDocumentLength(3));
         documentIndexHandler.closeFileChannel();
+        */
+
+        // test compression
+        byte[] intCompressed1 = Compression.variableByteCompression(1000);
+        byte[] intCompressed2 = Compression.variableByteCompression(10023402);
+
+        byte[] intCompressedReversed1 = Compression.reverseArray(intCompressed1);
+        byte[] intCompressedReversed2 = Compression.reverseArray(intCompressed2);
+
+        Compression.printBytes(intCompressedReversed1);
+        Compression.printBytes(intCompressedReversed2);
+
+        System.out.println(Compression.variableByteDecompression(intCompressed1));
+        System.out.println(Compression.variableByteDecompression(intCompressed2));
     }
+
+
 }
