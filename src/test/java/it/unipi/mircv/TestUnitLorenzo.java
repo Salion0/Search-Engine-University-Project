7ae11ba8f4f975bfa526913c70f23e84b41f9176
Loremazze <mazzeilorenzo35@virgilio.it>
2023-12-06 10:14:58 +0100

    Implementing Test Units for the Query package

--- src/test/java/it/unipi/mircv/TestUnitLorenzo.java
+++ src/test/java/it/unipi/mircv/TestUnitLorenzo.java
@@ -1,6 +1,7 @@
 package it.unipi.mircv;
 
 import it.unipi.mircv.File.DocumentIndexHandler;
+import it.unipi.mircv.File.InvertedIndexHandler;
 import it.unipi.mircv.Index.PostingElement;
 import it.unipi.mircv.Index.PostingListBlock;
 import it.unipi.mircv.Query.ScoreFunction;
@@ -17,15 +18,116 @@ import org.junit.jupiter.api.Assertions;
 public class TestUnitLorenzo {
     static PostingListBlock[] postingListBlocks = new PostingListBlock[5];
     static boolean[] endOfPostingListFlag = new boolean[5];
+    static int[] docFreqs = new int[5];
+    static int[] numBlockRead = new int[5];
+    static InvertedIndexHandler invertedIndexHandler;
+    static int[] offsets = new int[5];
 
     public static void main(String[] args) throws IOException {
+
+        testMinDocId();
+
+    }
+
+    private void updatePostingListBlock(int i) throws IOException {
+
+        int block_len = 3;
+        int elementToRead = docFreqs[i] - block_len*numBlockRead[i];
+
+        if (elementToRead > 0)
+        {
+            if(elementToRead > block_len )
+                elementToRead = block_len;
+
+            postingListBlocks[i] = invertedIndexHandler.getPostingList(
+                    offsets[i] + (block_len * numBlockRead[i]), elementToRead);
+
+            numBlockRead[i]++;
+        }
+        else
+            endOfPostingListFlag[i]=true;
+
+    }
+
+
+    private static int getMinDocId() {
+        int minDocId = collectionSize;  //valore che indica che le posting list sono state raggiunte
+
+        //find the current min doc id in the posting lists of the query terms
+        for (int i = 0; i < 5; i++){
+            if (endOfPostingListFlag[i]) continue;
+            int currentDocId = postingListBlocks[i].getCurrentDocId();
+            if(currentDocId<minDocId){
+                minDocId = currentDocId;
+            }
+        }
+        return minDocId;
+    }
+
+    public static void testUploadPostingListBlock() {
+
+        setLongerFirstPostingList();
+        setLongerSecondPostingList();
+        setLongerThirdPostingList();
+        setLongerFourthPostingList();
+        setLongerFifthPostingList();
+
+        int[] arraysOfDocIdsFirstPostingList = {2,5,6,13,14,18,21,24,25,29};
+        int[] arraysOfDocIdsSecondPostingList = {1,2,24,26,28,30,31,33,36,39};
+        int[] arraysOfDocIdsThirdPostingList = {7,10,11,46,50,56,57,58,66,68};
+        int[] arraysOfDocIdsFourthPostingList = {2,4,5,36,37,40,41,44,47,50};
+        int[] arraysOfDocIdsFifthPostingList = {1,7,9,11,12,15,19,20,22,24};
+
+        int[] arrayOfResults;
+        for (int j = 0; j < postingListBlocks.length; j++)
+            arrayOfResults = new int[postingListBlocks.length];
+            for (int i = 0; i < arraysOfDocIdsFourthPostingList.length; i++) {
+                arrayOfResults[i] =
+            }
+    }
+
+    public static void testMinDocId() {
+
+        int[] arraysOfMinDocIds = {1,2,4,5,6,7,9,10,11,24};
+        int[] docFreqs = {3,3,3,3,3};
+        int[] arraysOfResults = new int[arraysOfMinDocIds.length];
+
+        int minDocId;
+        int count = 0;
+        while((minDocId = getMinDocId()) != collectionSize) {
+            arraysOfResults[count] = minDocId;
+            for (int i =0; i<5;i++)
+            {
+                if (postingListBlocks[i].getCurrentDocId() == minDocId)
+                {
+                    docFreqs[i]--;
+                    if (docFreqs[i] == 0)
+                        endOfPostingListFlag[i] = true;
+                    else
+                        postingListBlocks[i].next();
+                }
+            }
+            count++;
+        }
+
+        // Assertion to check if arrays are equal
+        Assertions.assertArrayEquals(arraysOfResults, arraysOfMinDocIds); //, "Arrays are not equal");
+    }
+
+    public void setPostingListBlocksForTesting() throws IOException {
+
+        invertedIndexHandler = new InvertedIndexHandler();
         collectionSize = Integer.MAX_VALUE;
+
+        int count = 0;
         for (int i = 0; i < 5; i++) {
             postingListBlocks[i] = new PostingListBlock();
             endOfPostingListFlag[i] = false;
+            docFreqs[i] = 3;
+            numBlockRead[i] = 0;
+            offsets[i] = count;
+            count += 3;
         }
-
-
 
         postingListBlocks[0].addPostingElement(new PostingElement(2,4));
         postingListBlocks[0].addPostingElement(new PostingElement(5,1));
@@ -51,49 +153,70 @@ public class TestUnitLorenzo {
         postingListBlocks[4].addPostingElement(new PostingElement(7,1));
         postingListBlocks[4].addPostingElement(new PostingElement(9,1));
         postingListBlocks[4].setFields(3);
+    }
 
-        int[] arraysOfMinDocIds = {1,2,4,5,6,7,9,10,11,24};
-        int[] docFreqs = {3,3,3,3,3};
-        int[] arraysOfResults = new int[arraysOfMinDocIds.length];
-
-        int minDocId;
-        int count = 0;
-        while((minDocId = getMinDocId()) != collectionSize) {
-            arraysOfResults[count] = minDocId;
-            for (int i =0; i<5;i++)
-            {
-                if (postingListBlocks[i].getCurrentDocId() == minDocId)
-                {
-                    docFreqs[i]--;
-                    if (docFreqs[i] == 0)
-                        endOfPostingListFlag[i] = true;
-                    else
-                        postingListBlocks[i].next();
-                }
-            }
-            count++;
-        }
-
-
-
-        // Assertion to check if arrays are equal
-        Assertions.assertArrayEquals(arraysOfResults, arraysOfMinDocIds); //, "Arrays are not equal");
-
+    public static void setLongerFirstPostingList() {
+        postingListBlocks[0].addPostingElement(new PostingElement(13,4));
+        postingListBlocks[0].addPostingElement(new PostingElement(14,1));
+        postingListBlocks[0].addPostingElement(new PostingElement(18,3));
+        postingListBlocks[0].addPostingElement(new PostingElement(21,4));
+        postingListBlocks[0].addPostingElement(new PostingElement(24,1));
+        postingListBlocks[0].addPostingElement(new PostingElement(25,3));
+        postingListBlocks[0].addPostingElement(new PostingElement(29,3));
+        postingListBlocks[0].setFields(10);
+        docFreqs[0] = 10;
 
     }
 
+    public static void setLongerSecondPostingList() {
+        postingListBlocks[1].addPostingElement(new PostingElement(26,2));
+        postingListBlocks[1].addPostingElement(new PostingElement(28,3));
+        postingListBlocks[1].addPostingElement(new PostingElement(30,7));
+        postingListBlocks[1].addPostingElement(new PostingElement(31,2));
+        postingListBlocks[1].addPostingElement(new PostingElement(33,3));
+        postingListBlocks[1].addPostingElement(new PostingElement(36,7));
+        postingListBlocks[1].addPostingElement(new PostingElement(39,2));
+        postingListBlocks[1].setFields(10);
+        docFreqs[1] = 10;
+        offsets[1] = 10;
+    }
 
-    private static int getMinDocId() {
-        int minDocId = collectionSize;  //valore che indica che le posting list sono state raggiunte
+    public static void setLongerThirdPostingList() {
+        postingListBlocks[2].addPostingElement(new PostingElement(46,2));
+        postingListBlocks[2].addPostingElement(new PostingElement(50,3));
+        postingListBlocks[2].addPostingElement(new PostingElement(56,7));
+        postingListBlocks[2].addPostingElement(new PostingElement(57,2));
+        postingListBlocks[2].addPostingElement(new PostingElement(58,3));
+        postingListBlocks[2].addPostingElement(new PostingElement(66,7));
+        postingListBlocks[2].addPostingElement(new PostingElement(68,2));
+        postingListBlocks[2].setFields(10);
+        docFreqs[2] = 10;
+        offsets[2] = 20;
+    }
 
-        //find the current min doc id in the posting lists of the query terms
-        for (int i = 0; i < 5; i++){
-            if (endOfPostingListFlag[i]) continue;
-            int currentDocId = postingListBlocks[i].getCurrentDocId();
-            if(currentDocId<minDocId){
-                minDocId = currentDocId;
-            }
-        }
-        return minDocId;
+    public static void setLongerFourthPostingList() {
+        postingListBlocks[3].addPostingElement(new PostingElement(36,2));
+        postingListBlocks[3].addPostingElement(new PostingElement(37,3));
+        postingListBlocks[3].addPostingElement(new PostingElement(40,7));
+        postingListBlocks[3].addPostingElement(new PostingElement(41,2));
+        postingListBlocks[3].addPostingElement(new PostingElement(44,3));
+        postingListBlocks[3].addPostingElement(new PostingElement(47,7));
+        postingListBlocks[3].addPostingElement(new PostingElement(50,2));
+        postingListBlocks[3].setFields(10);
+        docFreqs[3] = 10;
+        offsets[3] = 30;
+    }
+
+    public static void setLongerFifthPostingList() {
+        postingListBlocks[4].addPostingElement(new PostingElement(11,2));
+        postingListBlocks[4].addPostingElement(new PostingElement(12,3));
+        postingListBlocks[4].addPostingElement(new PostingElement(15,7));
+        postingListBlocks[4].addPostingElement(new PostingElement(19,2));
+        postingListBlocks[4].addPostingElement(new PostingElement(20,3));
+        postingListBlocks[4].addPostingElement(new PostingElement(22,7));
+        postingListBlocks[4].addPostingElement(new PostingElement(24,2));
+        postingListBlocks[4].setFields(10);
+        docFreqs[4] = 10;
+        offsets[4] = 40;
     }
 }
