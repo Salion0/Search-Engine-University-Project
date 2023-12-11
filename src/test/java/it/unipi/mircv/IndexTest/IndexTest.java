e298f57e9af707f23bde4e6594ede5f80c206f94
Salvatore Patisso <120101001+Salion0@users.noreply.github.com>
2023-12-11 18:19:05 +0100

    push

--- src/test/java/it/unipi/mircv/IndexTest/IndexTest.java
+++ src/test/java/it/unipi/mircv/IndexTest/IndexTest.java
@@ -2,22 +2,18 @@ package it.unipi.mircv.IndexTest;
 
 import it.unipi.mircv.Config;
 import it.unipi.mircv.Utils;
-import it.unipi.mircv.file.DocumentIndexFileHandler;
 import it.unipi.mircv.file.InvertedIndexFileHandler;
 import it.unipi.mircv.file.LexiconFileHandler;
 import it.unipi.mircv.index.*;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.Assertions.*;
-
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import java.io.*;
-import java.nio.ByteBuffer;
-import java.nio.channels.FileChannel;
+
 import java.util.HashMap;
-import java.util.Random;
 
 import static it.unipi.mircv.Config.*;
 import static it.unipi.mircv.Utils.stemWord;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 public class IndexTest {
     //FILE name ---------------------------
@@ -127,7 +123,7 @@ public class IndexTest {
                 //INDEXING
                 index = new Index(blockFolder,testCollection,true);
                 //MERGING
-                BlockMerger.path = "dataTest/";
+                BlockMerger.path = "dataTest/spimiTest/";
                 BlockMerger.mergeBlocks(index.getNumberOfBlocks());
                 //LOAD MERGED LEXICON
                 mergedLexicon = loadMergedLexicon();
