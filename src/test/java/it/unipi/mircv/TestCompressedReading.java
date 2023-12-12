d779a9938bbd177b0f9c6c7294a19d6921edf5d3
Loremazze <mazzeilorenzo35@virgilio.it>
2023-12-12 15:59:09 +0100

    Implementing test units on test_collection

--- src/test/java/it/unipi/mircv/TestCompressedReading.java
+++ src/test/java/it/unipi/mircv/TestCompressedReading.java
@@ -1,6 +1,8 @@
 package it.unipi.mircv;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.util.zip.GZIPInputStream;
@@ -12,26 +14,19 @@ public class TestCompressedReading {
     static FileInputStream fis;
     static GZIPInputStream gzis;
     static InputStreamReader inputStreamReader;
-    public static void compressedRead(String[] args) throws IOException {
+
+    @Test
+    void compareCollectionReading() throws IOException {
         fis = new FileInputStream("collection.tar.gz");
         gzis = new GZIPInputStream(fis);
         inputStreamReader = new InputStreamReader(gzis, StandardCharsets.UTF_8);
         readerForCompressedCase = new BufferedReader(inputStreamReader); // reader for the compressed version
         reader = new BufferedReader(new FileReader("collection.tsv")); // reader for the uncompressed version
 
-        compareCollectionReading();
-    }
-
-    public static void compareCollectionReading() throws IOException {
         readerForCompressedCase.mark(1024); // 1024 represents how many bytes it can read before it becomes no longer valid
         String[] values = readerForCompressedCase.readLine().split("\t"); // to check for metadata
         readerForCompressedCase.reset(); // bring back the reader to the first line
         readerForCompressedCase.skip(values[0].length() - 1); // skip metadata
-
-        singlePassInMemoryIndexing();
-    }
-
-    private static void singlePassInMemoryIndexing() throws IOException {
 
         int count = 0;
         while (true)
